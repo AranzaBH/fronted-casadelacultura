79d721609c52c2cbb505c0df4114bdcbc97a38a9
Robertlasca <roberto.lasca123@gmail.com>
2025-03-02 01:03:47 -0600

    Se agrega agrega clase para la clase pictorica

--- src/app/components/obra-pictorica/ObraFonografica.ts
+++ src/app/components/obra-pictorica/ObraFonografica.ts
@@ -1,5 +1,22 @@
+import { Autor } from "../autor/Autor";
+import { CategoriaObra } from "../categoria-obra/CategoriaObra";
+import { Tecnica } from "../tecnicas/Tecnica";
 
 export class ObraPictorica{
-  
+  id?:number;
+    titulo?:string;
+    tituloOriginal?:string;
+    codigo?:string;
+    descripcion?:string;
+    dimension?:string;
+    localizacion?:string;
+    anioObra?:number;
+    tecnica?:Tecnica;
+    categoriaObra?:CategoriaObra;
+    fechaCreacion?:Date;
+    activo?:boolean;
+  autores?:Autor[];
+  imagenes?:string[];
+  urlImagenPortada?:string;
 
 }
