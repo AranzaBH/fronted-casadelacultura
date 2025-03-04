a9a2df7c09d7cf61ce29135e9ba99814b7838043
Robertlasca <roberto.lasca123@gmail.com>
2025-03-03 22:41:39 -0600

    Se agrega clase para las obras fonograficas

--- src/app/components/obra-fonografica/ObraFonografica.ts
+++ src/app/components/obra-fonografica/ObraFonografica.ts
@@ -1,5 +1,23 @@
+import { Autor } from "../autor/Autor";
+import { Estilos } from "../estilos/Estilos";
 
 export class ObraFonografica{
-  
-
+  id?:number;
+      titulo?:string;
+      lugar?:string;
+      anio?:number;
+      estilo?:Estilos;
+      origen?:string;
+      dimension?:string;
+      localizacion?:string;
+      fecha?:string;
+      clasificacion?:string;
+      ubicacion?:string;
+      observaciones?:string;
+      cantidad?:number;
+      fechaCreacion?:Date;
+      activo?:boolean;
+    autores?:Autor[];
+    imagenes?:string[];
+    urlImagenPortada?:string;
 }
